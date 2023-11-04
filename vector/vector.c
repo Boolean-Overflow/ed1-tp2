2bf0f1c649eacd6e9a0016fc6f6b886e118740ec
iKélsio <ikelsio@outlook.com>
2023-11-04 09:02:33 +0100

    kelsio methods done

--- vector/vector.c
+++ vector/vector.c
@@ -15,3 +15,52 @@ struct vector {
   point end;
   point coods;
 };
+
+vector* product_by_scalar(vector* v, float ß) {
+  if (!v) {
+    printf("\nVetor deve possuir coordenadas.\n");
+    return NULL;
+  }
+  if (!ß) {
+    printf("\n O escalar deve ser diferente de zero\n");
+    return NULL;
+  }
+  v->coods.x *= ß;
+  v->coods.y *= ß;
+  v->coods.z *= ß;
+
+  return v;
+}
+
+float scalar_product(vector* v1, vector* v2) {
+  return(
+    v1->coods.x * v2->coods.x +
+    v1->coods.y * v2->coods.y +
+    v1->coods.z * v2->coods.z
+    );
+}
+
+float vector_module(vector* v) {
+  if (!v) {
+    printf("\nVetor deve possuir coordenadas.\n");
+    return -1;
+  }
+  float x = v->coods.x, y = v->coods.y, z = v->coods.z;
+
+  return sqrt(x * x + y * y + z * z);
+}
+
+string relative_position(vector* v1, vector* v2) {
+  float angle = vectors_angle(v1, v2);
+  float scalarProduct = scalar_product(v1, v2);
+
+  if (scalarProduct == 0) return "ortogonais";
+  else if (scalarProduct == -1) return "colineares";
+  else if (cos(angle) == 1) return "paralelos";
+  else return "coincidentes";
+}
+
+void vector_delete(vector* v) {
+  if (!v) printf("\nVetor inexistente!\n");
+  else free(v), printf("\nVetor eliminado com sucesso!\n");
+}

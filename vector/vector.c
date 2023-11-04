1930890ceea4d20bb5f2f52d8a208cd99782821f
lukestarwalk <127775442+lukestarwalk@users.noreply.github.com>
2023-11-04 14:39:44 +0000

    Update vector.c

--- vector/vector.c
+++ vector/vector.c
@@ -63,3 +63,29 @@ void vector_delete(vector* v) {
   if (!v) printf("\nVetor inexistente!\n");
   else free(v), printf("\nVetor eliminado com sucesso!\n");
 }
+
+float distance(vector* v1, vector* v2){
+  float d = 0;
+  d = (float)(sqrt(pow(v2->coods.x - v1->coods.x, 2) + pow(v2->coods.y - v1->coods.y, 2) + pow(v2->coods.z - v1->coods.z, 2)));
+  return d;
+}
+
+float mix_product(vector* v1, vector* v2, vector* v3){
+  float result = 0;
+  result = scalar_product(v3, vectorial_product(v1, v2));
+  return result;
+}
+
+point interception(vector* v1, vector* v2){
+  
+  float incog1 = 0, incog2 = 0;
+  incog1 = ((v2->start.x)-(v1->start.x))/(v1->coods.x);
+  incog2 = ((v2->start.y)-(v1->start.y)-((v1->coods.y)*incog1))/(v1->coods.y - v2->coods.y);
+  incog1 += incog2;
+  point Point_Interc;
+  Point_Interc.x = (v1->start.x + (v1->coods.x)*incog1);
+  Point_Interc.y = (v1->start.y + (v1->coods.y)*incog1);
+  Point_Interc.z = (v1->start.z + (v1->coods.z)*incog1);
+  return Point_Interc;
+
+}
